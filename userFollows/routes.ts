d496feb90611443fa26cb3f5ceb879a53290029d
Atulya Miglani <atulyamiglani02@gmail.com>
2023-12-11 17:12:59 -0500

    user following relation

---
+++ userFollows/routes.ts
@@ -0,0 +1,34 @@
+import { Express, Request, Response } from "express";
+import * as dao from "./dao";
+
+function userRoutes(app: Express) {
+  app.get("/user/followers/:username", async (req: Request, res: Response) => {
+    const followers = await dao.findAllFollowers(req.params.username);
+    res.json(followers);
+  });
+
+  app.get("/user/following/:username", async (req: Request, res: Response) => {
+    const following = await dao.findAllFollowing(req.params.username);
+    res.json(following);
+  });
+
+  app.post("/user/follow", async (req: Request, res: Response) => {
+    const { followed, follower } = req.body as {
+      follower: string;
+      followed: string;
+    };
+    const relation = await dao.follow(follower, followed);
+    res.json(relation);
+  });
+
+  app.post("/user/unfollow", async (req: Request, res: Response) => {
+    const { followed, follower } = req.body as {
+      follower: string;
+      followed: string;
+    };
+    const deletedRelation = await dao.unfollow(follower, followed);
+    res.json(deletedRelation);
+  });
+}
+
+export default userRoutes;
